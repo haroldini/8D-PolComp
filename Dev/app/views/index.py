01a8f3b46c6cb9d9cff3fddf11ea00b8616dd73c
Haroldini <--global lemonboi42@gmail.com>
2023-02-04 23:28:06 +0000

    index page config, css templates

--- Dev/app/views/index.py
+++ Dev/app/views/index.py
@@ -4,4 +4,9 @@ v = Blueprint('index', __name__)
 
 @v.route("/", methods=["POST", "GET"])
 def index():
-    return render_template("pages/index.html")
+    data = {
+        "completed_count": 100,
+        "started_count": 200,
+        "recorded_count": 50,
+    }
+    return render_template("pages/index.html", data=data)
